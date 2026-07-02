8abea321bfc73e0f5f16c90ada722b164ded46c9
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-07-02 15:08:25 +0700

    feat: add food page html

---
+++ myArray.js
@@ -0,0 +1,58 @@
+let foodDTB = [
+  {
+    id: 1,
+    name: "Pizza",
+    price: 10,
+    img: "https://img.magnific.com/free-psd/delicious-pepperoni-pizza-culinary-delight_632498-24206.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  {
+    id: 2,
+    name: "Burger",
+    price: 5,
+    img: "https://img.wongnai.com/p/1920x0/2025/09/11/b17d6b5428204bb8a4b79d880ae2c07a.jpg",
+  },
+  {
+    id: 3,
+    name: "Pasta",
+    price: 8,
+    img: "https://www.eatingwell.com/thmb/s-b7-fRUaSso8SjiNCTI9_qthzw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/CKL_cherrytomatopasta-1x1-440bd3179ca44872bae3cc5f9122a21a.jpg",
+  },
+  {
+    id: 4,
+    name: "Salad",
+    price: 6,
+    img: "https://www.eatingwell.com/thmb/xzR1INWu-qKNgOqwG4cIMERqsMo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eat-the-rainbow-chopped-salad-with-basil-mozzarella-beauty-184-278133-4000x4000-ef8f3f0ad7134d2b860c51f5e7b38ce5.jpg",
+  },
+];
+
+let staffMembers = [
+  {
+    id: 1,
+    name: "Thiem",
+    age: 30,
+    role: "Manager",
+    img: "https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fHww",
+  },
+  {
+    id: 2,
+    name: "Nook",
+    age: 25,
+    role: "Chef",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlwlKOjBWeKpkPljQR4EQVmxCpOR_yN-MNOzTFdn6iDwJE5SGJ5YOOGKM&s=10",
+  },
+  {
+    id: 3,
+    name: "Eye",
+    age: 28,
+    role: "Waiter",
+    img: "https://img.magnific.com/free-photo/happy-young-woman-standing-isolated-white-wall_171337-18037.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  {
+    id: 4,
+    name: "Pluem",
+    age: 32,
+    role: "Cashier",
+    img: "https://img.magnific.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+];
+// console.log(foodDTB);
